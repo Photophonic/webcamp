e0f43db1fba0e989a4aab8a56104933d6dd5b2ea
Seth Martin <sgm13555@gmail.com>
2023-07-19 15:51:09 -0500

    Section 18 Objects

---
+++ s17_Array/script.js
@@ -0,0 +1,31 @@
+let students = [];
+
+let colors = ["red", "blue", "yellow"];
+
+console.log(colors);
+
+let stuff = [true, "blue", 88];
+
+let lottoNumbers = [6, 5, 3, 77, 2, 94];
+
+console.log(stuff[0]);
+
+stuff.unshift("test");
+
+console.log(stuff);
+
+stuff.push("push");
+console.log(stuff);
+
+stuff.pop();
+console.log(stuff);
+
+const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"];
+
+planets.shift();
+
+planets.push("Saturn");
+
+planets.unshift("Mercury");
+
+console.log(planets);
