8d51d0b4a884997dc38fad83c45789f13c1481a5
Seth Martin <sgm13555@gmail.com>
2023-07-25 12:57:43 -0500

    loops

---
+++ s19_Functions/script.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 25; i >= 0; i -= 5) {
+  console.log(i);
+}
+
+const animals = ["lions", "tigers", "bears"];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
