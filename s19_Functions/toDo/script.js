4f90a625fd2c42384f214d67d31de0348b4427c5
Seth Martin <sgm13555@gmail.com>
2023-07-27 15:07:38 -0500

    Finish list

--- s19_Functions/toDo/script.js
+++ s19_Functions/toDo/script.js
@@ -2,7 +2,7 @@ let input = console.log("What would you like to do?");
 
 const todos = ["Collect Eggs", "Clean house"];
 
-while (input !== "quit" && input !== "q") {
+while (input !== "quit") {
   input = prompt("What would you like to do?");
   if (input === "list") {
     console.log("*************************");
@@ -12,10 +12,19 @@ while (input !== "quit" && input !== "q") {
     console.log("*************************");
   } else if (input === "new") {
     const newTodo = prompt("Enter a new todo item.");
+
     todos.push(newTodo);
     console.log(`${newTodo} added to the list`);
   } else if (input === "delete") {
-    const delTodo = prompt("What are you trying to delete?");
+    // use parseInt to convert to integer '23' -> 23
+    const delTodo = parseInt(prompt("What are you trying to delete?"));
+
+    // if NOT NaN then delete
+    if (!Number.isNaN(delTodo)) {
+      //will delete at the provided index for 1 object (x,1)
+      const deleted = todos.splice(delTodo, 1);
+      console.log(`${deleted} was deleted from the list`);
+    }
   }
 }
 
