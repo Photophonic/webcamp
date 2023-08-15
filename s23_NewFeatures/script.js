0f49ed87ded93f57d5bc95fa7854424de8ba5a47
Seth Martin <sgm13555@gmail.com>
2023-08-15 15:59:20 -0500

    Destructuring

--- s23_NewFeatures/script.js
+++ s23_NewFeatures/script.js
@@ -68,3 +68,49 @@ const newUser = {
 };
 
 console.log(newUser);
+
+// rest paramater, collects all remaining arguments into an array/single parameter
+// will not work with arrow functions.
+function sum(...nums) {
+  console.log(nums);
+
+  // pass in a callback function that returns the accumulated amount of prices
+  return nums.reduce((total, element) => total + element);
+}
+
+// first two are pulled and then the remaining arguments are bunched into the ...others
+function raceResults(gold, silver, ...others) {
+  console.log(`Gold goes to ${gold}`);
+  console.log(`Silver goes to ${silver}`);
+  console.log(`The rest are ${others} `);
+}
+
+raceResults("Bill", "Pete", "Stu", "Ron", "John", "Mark");
+
+// Destructuring. A means to separate values in an array to separate variables
+const scores = [99, 96, 80, 75, 70, 65, 60];
+
+/* Old Way:
+const highScore = [0];
+const runnerUp = [1];
+*/
+
+// create three variables off the scores array
+const [hightScore, runnerUp, ...others] = scores;
+console.log(hightScore);
+console.log(runnerUp);
+console.log(others);
+
+// to get an element out of an object with destructuring
+// use property name: New Name to change the name of the new variable
+// to add default values
+const {
+  email,
+  password,
+  username,
+  userName,
+  isAdmin: Admin,
+  Country = "US",
+} = newUser;
+// this will create standalone variables of the element from the object
+console.log(Country);
