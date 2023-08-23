6257cb95caceacb7a63ec9ea34baad3db08d750a
Seth Martin <sgm13555@gmail.com>
2023-08-23 15:50:31 -0500

    Dom stuff

--- s24_DOM/Selecting/app.js
+++ s24_DOM/Selecting/app.js
@@ -1,18 +1,24 @@
-const allImages = document.getElementsByTagName('img');
+// const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
+// for (let img of allImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
 
+// const squareImages = document.getElementsByClassName('square');
 
-const squareImages = document.getElementsByClassName('square');
+// for (let img of squareImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
+// const links = document.querySelectorAll('p a');
 
-const links = document.querySelectorAll('p a');
+// for (let link of links) {
+//     console.log(link.href)
+// }
 
-for (let link of links) {
-    console.log(link.href)
-}
+// to select an element by the ID tag. Needs to match an ID on the HTML
+const banner = document.getElementById("banner");
+//console.dir(banner) to see this object in console.
+
+const toc = document.getElementById("toc");
+console.log(toc);
