a3d2b780f4fb680251c909841c679c1a95fd8e25
Seth Martin <sgm13555@gmail.com>
2023-09-22 15:44:23 -0500

    Events wrap-up

--- s25_DOME_Events/ChangeAndInputEvents/app.js
+++ s25_DOME_Events/ChangeAndInputEvents/app.js
@@ -1,10 +1,40 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
-// input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
-// })
+// this only fires when the value is changed after the fact
+// input.addEventListener("change", function () {
+//   console.log("loooooooog");
+// });
 
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
-})
+//input event
+input.addEventListener("input", function (e) {
+  //  This will capture the value from the input field
+  //  and will update the h1 value.
+  h1.innerText = input.value;
+});
+
+// <head>
+//     <title>Input Event</title>
+//     <!--LEAVE THESE LINES ALONE, PLEASE! THEY MAKE THE LIVE PREVIEW WORK!-->
+//     <script src="node_modules/babel-polyfill/dist/polyfill.js" type="text/javascript"> </script>
+//     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+// </head>
+
+// <body>
+//     <h1>Enter Your Username</h1>
+//     <input type="text" id="username">
+// </body>
+
+// const input = document.querySelector('#username');
+// const h1 = document.querySelector('h1');
+
+// input.addEventListener('input', function(){
+//          const message = ('Welcome, '+input.value);
+
+//          if (input.value.length === 0){
+//              h1.innerText = 'Enter Your Username'
+//          } else {
+//              h1.innerText = message ;
+//          }
+//     })
--- s25_DOME_Events/FormEvents/app.js
+++ s25_DOME_Events/FormEvents/app.js
@@ -18,3 +18,46 @@ form.addEventListener("submit", function (e) {
   // reset the input form
   form.reset();
 });
+
+//Lab Exercise
+// <!DOCTYPE html>
+
+// <head>
+//     <title>Grocery List</title>
+//     <!--LEAVE THESE LINES ALONE, PLEASE! THEY MAKE THE LIVE PREVIEW WORK!-->
+//     <script src="node_modules/babel-polyfill/dist/polyfill.js" type="text/javascript"> </script>
+//     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+// </head>
+
+// <body>
+//     <h1>Grocery List</h1>
+//     <form action="/nowhere">
+//         <label for="product">Enter A Product</label>
+//         <input type="text" id="product" name="product">
+//         <label for="qty">Enter A Quantity</label>
+//         <input type="number" id="qty" name="qty">
+//         <button>Submit</button>
+//     </form>
+
+//     <ul id="list"></ul>
+// </body>
+
+// </html>
+
+// // Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+// const form = document.querySelector('form');
+// const prodInput = document.querySelector('#product');
+// const qtyInput = document.querySelector('#qty');
+// const ul = document.querySelector('#list')
+
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+
+//    const prod = prodInput.value;
+//    const qty = qtyInput.value;
+//    const newLi = document.createElement('LI');
+//    newLi.innerText = (`${qty} ${prod}`);
+//    ul.appendChild(newLi);
+//    form.reset();
+// });
